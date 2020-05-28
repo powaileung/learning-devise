eefa8c8cab83c8e874ce08712c65e697deafcd98
Powai Leung <pwleung1489@gmail.com>
2020-05-28 03:47:29 +0000

    Input signing in other than email in learning-devise app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #these codes are written inside configure_permitted_parameters function
+  protected
+    def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) {|u| u.permit(:signin)}
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email, :username, :password, :password_confirmation)}
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attr_accessor :signin
+  def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      login = conditions.delete(:signin)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.strip.downcase}]).first  
+  end    
+  validates :username, :uniqueness => {:case_sensitive => false}
+         
 end

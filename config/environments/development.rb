9dd9dfedee1735101d46c57869ab868646852312
Powai Leung <pwleung1489@gmail.com>
2020-06-03 05:27:14 +0000

    Input email confirmable function in learning-devise app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,13 +1,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com", 
     port: 587,
     domain: "gmail.com",
-    user_name: "californiaautocenter316@gmail.com",
-    password: "Hillcrest94030",
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }

64b520d463a714041dc27b74fbb15e43c6e5d191
Powai Leung <pwleung1489@gmail.com>
2020-06-03 04:20:33 +0000

    Input email confirmable function in learning-devise app

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   attr_accessor :signin
-  def self.find_first_by_auth_conditions(warden_conditions)
-      conditions = warden_conditions.dup
-      login = conditions.delete(:signin)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.strip.downcase}]).first  
-  end    
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:signin)
+    where(conditions).where(["lower(username) = :value OR lower(email) = 
+    :value", {:value => login.strip.downcase }]).first
+end 
   validates :username, :uniqueness => {:case_sensitive => false}
          
 end

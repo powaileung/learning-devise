d3a9b0f4d0a46d4714154a63b15cd34eaef0820d
Powai Leung <pwleung1489@gmail.com>
2020-06-03 04:58:49 +0000

    Input email confirmable function in learning-devise app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com", 
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+  
   config.action_mailer.default_url_options = { host: 'learning-devise-powai-leung.herokuapp.com' }
 end
